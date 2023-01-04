fac292b464448d8965764e20d8d51af3b61e8a11
mar69287 <marcoa.ruiz97@gmail.com>
2023-01-04 14:53:22 -0800

    completed straight function

--- layout/layout.js
+++ layout/layout.js
@@ -87,61 +87,14 @@ renderDeck()
                 faceVal = 3
                 })
             }
-            createPlayerArray() 
-            createPlayerHand()  
+
+            createPlayerArray()
+            //console.log(checkFlush())
+            removeSuits()
+            sortPlayerArray()
+            //console.log(checkStraight())
         }
 
-        function createPlayerArray() {
-            playerArray.push(deck[0].face)
-            playerArray.push(deck[2].face)
-            for (let i = 4; i < 9; i++) {
-                playerArray.push(deck[i].face)
-            }
-            console.log(playerArray)
-        }
-
-        function createPlayerHand() {
-            let spadeCounter = 0;
-            let clubCounter = 0;
-            let heartCounter = 0;
-            let diamondCounter = 0;
-            for (let i = 0; i < 7; i++) {
-                if (playerArray[i].includes("s")) {
-                    spadeCounter++
-                }
-                if (spadeCounter >= 5) {
-                    const playerHand = playerArray.filter(function(spade) { return spade.includes("s")})
-                    console.log(playerHand)
-                }
-            }
-            for (let i = 0; i < 7; i++) {
-                if (playerArray[i].includes("c")) {
-                    clubCounter++
-                    if (clubCounter >= 5) {
-                        const playerHand = playerArray.filter(function(club) { return club.includes("c")})
-                        console.log(playerHand)
-                    }
-                }
-            }
-            for (let i = 0; i < 7; i++) {
-                if (playerArray[i].includes("h")) {
-                    heartCounter++
-                    if (heartCounter >= 5) {
-                        const playerHand = playerArray.filter(function(heart) { return heart.includes("h")})
-                        console.log(playerHand)
-                    }
-                }    
-            }
-            for (let i = 0; i < 7; i++) {
-                if (playerArray[i].includes("d")) {
-                    diamondCounter++
-                    if (diamondCounter >= 5) {
-                        const playerHand = playerArray.filter(function(diamond) { return diamond.includes("d")})
-                        console.log(playerHand)
-                    }
-                }    
-            }
-        }
     });
 
 
@@ -157,16 +110,174 @@ foldBtn.addEventListener("click", function(evt) {
     playerArray.splice(0, playerArray.length)
 });
 
+function createPlayerArray() {
+    playerArray.push(deck[0].face)
+    playerArray.push(deck[2].face)
+    for (let i = 4; i < 9; i++) {
+        playerArray.push(deck[i].face)
+    }
+    console.log(playerArray)
+}
 
-function checkStraight(arr) {
-    const result = arr.every(function(value, idx) {
-        if(value === array[0]) {
-          return true
+function checkStraight() {
+let size = playerArray.length
+
+    if (size === 7) {
+        if (playerArray[3] - playerArray[4] !== 1) {
+            if (playerArray[3] === playerArray[4]) {
+                playerArray.splice(3, 1)
+            }
         } 
-        if(arr[idx-1] - value === 1) {
-          return true
-        } else return false  
-      })
-    console.log(result)
-  }
-  checkStraight(array)
+        if (playerArray[2] - playerArray[3] !== 1) {
+            if (playerArray[2] === playerArray[3]) {
+                playerArray.splice(3, 1)
+            }
+        }
+    }
+    console.log(size)
+
+    if (playerArray.length > 5) {
+  
+        if (playerArray[0] - playerArray[1] !== 1) {
+          if (playerArray[0] === playerArray[1]) {
+                playerArray.splice(0, 1)
+                if (playerArray[0] - playerArray[1] !== 1) {
+                    playerArray.splice(0, 1)
+                }
+          }else if (playerArray[1] - playerArray[2] !== 1) {
+                
+               
+                    playerArray.splice(0, 2)
+                
+          }else {
+                playerArray.splice(0, 1)
+          }        
+        }else if (playerArray[1] - playerArray[2] !== 1) {
+            if (playerArray[1] === playerArray[2]) {
+                playerArray.splice(1, 1)
+            }else {
+                playerArray.splice(0, 2)
+            }
+        }
+
+        console.log(playerArray)
+        size = playerArray.length
+
+        if (playerArray[size-3] - playerArray[size-2] !== 1) {
+            if (playerArray[size-3] === playerArray[size-2]) {
+                if (playerArray[size-2] === playerArray[size-1]) {
+                    playerArray.splice(size-2, 2)
+                }else if (playerArray[size-2] - playerArray[size-1] !== 1) {
+                    playerArray.splice(size-2, 2)
+                }else if (playerArray[size-2] - playerArray[size-1] === 1) {
+                    playerArray.splice(size-2, 1)
+                }
+            } else {
+                playerArray.splice(size-2, 2)
+            }
+        }
+        if (playerArray[size-2] - playerArray[size-1] !== 1) {
+            playerArray.splice(size-1, 1)
+        }
+    }
+
+    console.log(playerArray)
+    let result = null;
+
+    if (playerArray.length > 4) {
+      
+       result = playerArray.every(function(value, idx) {
+            if(value === playerArray[0]) {
+              return true
+            } 
+            if(playerArray[idx-1] - value === 1) {
+              return true
+            } else return false  
+        })
+      
+    } else result = false
+    console.log("you have a straight: " + result)
+    
+}
+
+function checkFlush() {
+    let spadeCounter = 0;
+    let clubCounter = 0;
+    let heartCounter = 0;
+    let diamondCounter = 0;
+    for (let i = 0; i < 7; i++) {
+        if (playerArray[i].includes("s")) {
+            spadeCounter++
+        }
+            if (spadeCounter >= 5) {
+                const playerHand = playerArray.filter(function(spade) { return spade.includes("s")})
+                console.log(playerHand)
+                return true
+            }
+    }
+    for (let i = 0; i < 7; i++) {
+        if (playerArray[i].includes("c")) {
+            clubCounter++
+            if (clubCounter >= 5) {
+                const playerHand = playerArray.filter(function(club) { return club.includes("c")})
+                console.log(playerHand)
+                return true
+            }
+        }
+    }
+    for (let i = 0; i < 7; i++) {
+        if (playerArray[i].includes("h")) {
+            heartCounter++
+            if (heartCounter >= 5) {
+                const playerHand = playerArray.filter(function(heart) { return heart.includes("h")})
+                console.log(playerHand)
+                return true
+            }
+        }    
+    }
+    for (let i = 0; i < 7; i++) {
+        if (playerArray[i].includes("d")) {
+            diamondCounter++
+            if (diamondCounter >= 5) {
+                const playerHand = playerArray.filter(function(diamond) { return diamond.includes("d")})
+                console.log(playerHand)
+                return true
+            }     
+        }   
+    }
+    return false
+}
+
+function removeSuits() {
+    playerArray.forEach(function(value,idx) {
+        playerArray[idx] = value.substring(1, playerArray.length)
+    })
+    console.log(playerArray)
+}
+
+function sortPlayerArray() {
+    playerArray.forEach(function(value, idx) {
+        if (isNaN(value) !== true) {
+          playerArray[idx] = parseInt(value, 10)
+        }
+        if (playerArray[idx] === "J") {
+          playerArray[idx] = 11
+        }
+        if (playerArray[idx] === "Q") {
+          playerArray[idx] = 12
+        }
+        if (playerArray[idx] === "K") {
+          playerArray[idx] = 13
+        }
+        if (playerArray[idx] === "A") {
+          playerArray[idx] = 14
+        }
+    })
+
+    playerArray.sort(function(a, b) { return b - a })
+    console.log(playerArray)
+}
+
+function checkFourKind() {
+    
+}
